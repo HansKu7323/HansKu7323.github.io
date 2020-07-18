3d8c35a3a34bc70d56cf85fa192b0d8f2428b5fb
HansKu7323 <boy7323@gmail.com>
2020-07-18 13:31:16 +0900

    20200718part4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+// slider
+$(function(){
+	var setImg = '.slider';
+	var fadeSpeed = 1500;
+	var switchDelay = 5000;
+ 
+	$(setImg).children('img').css({opacity:'0'});
+	$(setImg + ' img:first').stop().animate({opacity:'1',zIndex:'20'},fadeSpeed);
+ 
+	setInterval(function(){
+		$(setImg + ' :first-child').animate({opacity:'0'},fadeSpeed).next('img').animate({opacity:'1'},fadeSpeed).end().appendTo(setImg);
+	},switchDelay);
+});
+
+
+
+
+
 // back to top
 const $win = $(window);
 const back = $('#back');
@@ -18,3 +36,5 @@ $(function () {
     
     $('html, body').animate({scrollTop: 0}, 200);
   });
+
+
